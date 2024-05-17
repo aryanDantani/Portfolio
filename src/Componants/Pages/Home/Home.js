0c8d014d06bb99818036c3b253281778d7c2619c
AryanDantani <aryandantani007@gmail.com>
2024-05-17 12:32:05 +0530

    1-Protfolio Commit

---
+++ src/Componants/Pages/Home/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <>
+      <div class="context">
+        <h3>Hi There !</h3>
+        <h1>I'M Aryan Dantani</h1>
+      </div>
+
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/Componants/Pages/PageContants/Contant.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from '../Home/Home'
+
+const Contant = () => {
+  return (
+    <div>
+        <div>
+            <Home/>
+        </div>
+    </div>
+  )
+}
+
+export default Contant
