90f5849213bda1db8b0e9cd709c03d40fcdb5d17
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-02-18 22:07:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,33 +10,41 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import gsapLoader from './gsapLoader';
 
 const router = createBrowserRouter([
+
   {
+
       path: "/",
+
       element: <App />,
+
       children: [
+
           {
               element: <Home />,
               path: "/",
               loader: gsapLoader
           },
+
           {
               element: <AboutUs />,
               path: "/aboutus",
               loader: gsapLoader
           },
+
           {
             element: <ContactUs />,
             path: "/contactus",
             loader: gsapLoader
         }
+
       ]
+
   }
+
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(<RouterProvider router={router} />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
