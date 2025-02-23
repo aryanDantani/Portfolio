fe4c68904bf2ef34951655da107b82f992ca1cd3
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-02-23 22:54:59 +0530

    Update NavigationBar.js

--- src/Componants/Navigation/NavigationBar.js
+++ src/Componants/Navigation/NavigationBar.js
@@ -7,7 +7,9 @@ const NavigationsSec = () => {
 
   return (
     <nav class="navbar">
+
       <ul class="navbar__menu">
+
         <li
           class={
             location.pathname.includes("/home")
@@ -15,11 +17,14 @@ const NavigationsSec = () => {
               : "navbar__item"
           }
         >
+
           <a href="/home" class="navbar__link">
             <i class="fa-solid fa-house" />
             <span>Home</span>
           </a>
+
         </li>
+
         <li
           class={
             location.pathname.includes("/message")
@@ -27,11 +32,14 @@ const NavigationsSec = () => {
               : "navbar__item"
           }
         >
+
           <a href="/message" class="navbar__link">
             <i class="fa-solid fa-message" />
             <span>Messages</span>
           </a>
+
         </li>
+
         <li
           class={
             location.pathname.includes("/customers")
@@ -39,11 +47,14 @@ const NavigationsSec = () => {
               : "navbar__item"
           }
         >
+
           <a href="/customer" class="navbar__link">
             <i class="fa-solid fa-people-group" />
             <span>Customers</span>
           </a>
+
         </li>
+
         <li
           class={
             location.pathname.includes("/project")
@@ -51,23 +62,28 @@ const NavigationsSec = () => {
               : "navbar__item"
           }
         >
+
           <a href="/project" class="navbar__link">
             <i class="fa-solid fa-passport" />
             <span>Projects</span>
           </a>
+
         </li>
+
         <li class="navbar__item">
           <a href="#" class="navbar__link">
             <i class="fa-solid fa-wrench" />
             <span>Resources</span>
           </a>
         </li>
+
         <li class="navbar__item">
           <a href="#" class="navbar__link">
             <i class="fa-solid fa-handshake-angle" />
             <span>Help</span>
           </a>
         </li>
+
         <li
           class={
             location.pathname.includes("/settings")
@@ -80,28 +96,36 @@ const NavigationsSec = () => {
             <span>Settings</span>
           </a>
         </li>
+
       </ul>
+
       <div class="pr-section">
+
         <ul class="navbar__menu">
+
           <li class="navbar__item">
             <a href="#" class="navbar__link">
               <i class="fa-brands fa-github" />
               <span>Github</span>
             </a>
           </li>
+
           <li class="navbar__item">
             <a href="#" class="navbar__link">
               <i class="fa-brands fa-linkedin" />
               <span>Linkdin</span>
             </a>
           </li>
+
           <li class="navbar__item">
             <a href="#" class="navbar__link">
               <i class="fa-brands fa-instagram" />
               <span>Instagram</span>
             </a>
           </li>
+
         </ul>
+
       </div>
     </nav>
   );
