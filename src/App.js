a9c83445c013d9873d22e3d249c6f42567d2cf93
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2025-10-03 23:21:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,1 @@
-import { Outlet } from "react-router-dom";
-import "./App.css";
-import Sidebar from "./Componants/Sidebar/Sidebar";
-import Footer from "./Componants/Pages/Footer/Footer";
 
-function App() {
-
-  return (
-    <div className="App">
-      <Sidebar />
-      <main>
-        <Outlet />
-      </main>
-    </div>
-  );
-}
-
-export default App;
